da581a0da4285d909ec747f3c8c449ef8a43907c
Hesam <mohebi.hesam@gmail.com>
2022-03-17 19:19:08 +0330

    solved with sulotion

--- leetCode/AmazoneInterveiw/Search Suggestions System.py
+++ leetCode/AmazoneInterveiw/Search Suggestions System.py
@@ -35,6 +35,39 @@ def way1(products, searchWord):
             lres.append([])
     return lres
 
+
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+# In a sorted list of words,
+# for any word A[i],
+# all its sugested words must following this word in the list.
+#
+# For example, if A[i] is a prefix of A[j],
+# A[i] must be the prefix of A[i + 1], A[i + 2], ..., A[j]
+
+
+
+import bisect
+def way2(products, searchWord):
+    products.sort()
+    res, prefix, i = [], '', 0
+    for c in searchWord:
+        prefix += c
+
+        i = bisect.bisect_left(products, prefix)
+        # Locate the insertion point for prefix in products to maintain sorted order.
+
+        tmp = []
+        for w in products[i:i+3]:
+            if w.startswith(prefix):
+                tmp.append(w)
+        res.append(tmp)
+        # res.append([w for w in A[i:i + 3] if w.startswith(prefix)])
+    return res
+
 products = ["mobile","mouse","moneypot","monitor","mousepad"]
 searchWord = "mouse"
 
@@ -43,9 +76,10 @@ searchWord = "mouse"
 #
 # products = ["bags","baggage","banner","box","cloths"]
 # searchWord = "bags"
-#
-products = ["havana"]
-searchWord = "tatiana"
+# #
+# products = ["havana"]
+# searchWord = "tatiana"
 
-print(way1(products, searchWord))
+# print(way1(products, searchWord))
+print(way2(products, searchWord))
 
